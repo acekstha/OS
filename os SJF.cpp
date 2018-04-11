00f7c785b08b47ec0acf475b06a824753e96964f
acekstha <33475359+acekstha@users.noreply.github.com>
2018-04-11 22:02:18 +0530

    Update os SJF.cpp
    K1624
    B33

--- os SJF.cpp
+++ os SJF.cpp
@@ -1,14 +1,4 @@
-/*
-10)Design a scheduler with multilevel queue having two queues
- which will schedule the processes on the basis of
-  pre-emptive shortest remaining processing time first algorithm (SROT) 
-  followed by a scheduling in which each process will get 2 units of time to execute.
- Also note that queue 1 has higher priority than queue 2. 
- Consider the following set of processes (for reference)with
- their arrival times and the CPU burst times in milliseconds. 
-Calculate the average turnaround time and average waiting time for each process.
-The input for number of processes  and their arrival time, burst time should be given by the user.
-*/
+
 
 #include<stdio.h>
 #include<conio.h>
